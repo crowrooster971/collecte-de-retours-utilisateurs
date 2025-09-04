2a6d7e2a5b7593a6c2e9f5c207e0730fb915b3b6
crowrooster971 <wanlishajna@hotmail.com>
2025-09-04 20:43:53 +0300

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l\u0027application de collecte de retours utilisateurs !');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur écoutant sur http://localhost:${port}`);
+});
